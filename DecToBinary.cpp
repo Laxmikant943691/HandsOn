692f244620bd8c82f22f90e5cb4f96c926f22572
Laxmikant943691 <publicuseld94@gmail.com>
2025-01-11 23:22:22 +0530

    Adding files

---
+++ DecToBinary.cpp
@@ -0,0 +1,23 @@
+# include <iostream>
+using namespace std;
+
+int decToBinary(int decNum)
+{
+  int pow = 1,ans = 0;
+  while(decNum>0)
+  {
+    int rem = decNum%2;
+    decNum/=2;
+    ans += rem*pow;
+    pow *= 10;
+  }
+  return ans;
+}
+int main() {
+    for (int i= 1;i<=5;i++)
+    {
+        cout << decToBinary(i) <<endl;
+        
+    }
+    return 0;
+}
