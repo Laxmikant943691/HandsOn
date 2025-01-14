6feecc597e560d610dfbc09be9da4fa2a110cf01
Laxmikant943691 <publicuseld94@gmail.com>
2025-01-14 17:37:52 +0530

    Adding APttern code

---
+++ NumSquarepattern.cpp
@@ -0,0 +1,74 @@
+# include <iostream>
+using namespace std;
+
+int main() 
+    {   
+        cout << "1234 Sqr patter:" <<endl;
+        cout << endl;
+        for (int i=1;i<5;i++)
+            {
+              for (int j=1;j<5;j++){
+                cout << j;
+              }
+              cout << endl;
+            } 
+        
+        cout << endl;
+        cout << "ABCD Sqr patter:" <<endl;
+        cout << endl;
+        
+        for (int i=1;i<5;i++)
+            { 
+              char  k = 'A';
+              for (int j=1;j<5;j++){
+                cout << k;
+                k++;
+              }
+              cout << endl;
+            }
+        cout << endl;
+        cout << "* Reverse trngle patter:" <<endl;
+        cout << endl;
+        for (int i=4;i>0;i--)
+            { 
+              for (int j=0;j<i;j++){
+                cout << "*";
+              }
+              cout << endl;
+            }
+
+        cout << endl;
+        cout << "* trngle patter:" <<endl;
+        cout << endl;
+        for (int i=0;i<5;i++)
+            { 
+              for (int j=0;j<i;j++){
+                cout << "*";
+              }
+              cout << endl;
+            }
+        
+        cout << endl;
+        cout << "Num trngle patter:" <<endl;
+        cout << endl;
+        int nm=1;
+        for (int i=0;i<5;i++)
+            { 
+              for (int j=0;j<i;j++){
+                cout << nm <<" ";
+                nm++;
+              }
+              cout << endl;
+            }
+        
+        cout << endl;
+        cout << "* 1,21,321,4321 trngle patter:" <<endl;
+        cout << endl;
+        for (int i=0;i<5;i++)
+            { 
+              for (int j=i+1;j>0;j--){
+                cout << j <<" "; 
+              }
+              cout << endl;
+            }
+    }
